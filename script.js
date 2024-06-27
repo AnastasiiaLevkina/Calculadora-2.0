c366ef03a25946e9f61aa3a4d8dcc91e66405a26
picuu <carloscapoforcada@gmail.com>
2024-06-27 10:56:05 +0200

    refactor controlInputNumberCoherency conditional

--- script.js
+++ script.js
@@ -37,34 +37,40 @@ function enterDigit(digit) {
     selectedNum = "";
     displayCleanNeeded = false;
   }
+
   if (!reachedMaxDigits) {
     if (digit === ",") {
       handleCommaInput();
     } else {
       selectedNum += digit;
     }
+
     if (operatorAndCommandButtonsDisabled) {
       enableOperatorAndCommandButtons();
     }
+
     controlInputNumberCoherency();
     printOutputAndControlLength();
   }
 }
 
 function controlInputNumberCoherency() {
-  if (selectedNum.length > String(parseFloat(selectedNum)).length) {
-    if (selectedNum != "-0" && !selectedNum.includes(",")) {
-      if (selectedNum[0] === "-") {
-        selectedNum = "-" + selectedNum.slice(2, selectedNum.length);
-      } else {
-        selectedNum = selectedNum.slice(1, selectedNum.length);
-      }
+  const hasExtraCharacters = selectedNum.length > String(parseFloat(selectedNum)).length;
+  const isNotNegativeZero = selectedNum != "-0";
+  const hasNoDecimals = !selectedNum.includes(",");
+
+  if (hasExtraCharacters && isNotNegativeZero && hasNoDecimals) {
+    if (selectedNum[0] === "-") {
+      selectedNum = "-" + selectedNum.slice(2, selectedNum.length);
+    } else {
+      selectedNum = selectedNum.slice(1, selectedNum.length);
     }
   }
 }
 
 function printOutputAndControlLength() {
   calcDisplay.updateDisplayOutput(selectedNum);
+
   if (selectedNum.length === maxDigits) {
     reachedMaxDigits = true;
     disableDigitInputButtons();
@@ -78,6 +84,7 @@ function handleCommaInput() {
     } else {
       selectedNum += ",";
     }
+
     hasComma = true;
     commaButton.disableDOMButton();
   }
@@ -87,10 +94,13 @@ function disableDigitInputButtons() {
   numButtons.forEach((button) => {
     button.disableDOMButton();
   });
+
   commaButton.disableDOMButton();
+
   if (selectedNum[0] != "-") {
     changeSignButton.disableDOMButton();
   }
+
   inputDigitButtonsDisabled = true;
 }
 
@@ -98,6 +108,7 @@ function enableDigitInputButtons() {
   numButtons.forEach((button) => {
     button.enableDOMButton();
   });
+
   commaButton.enableDOMButton();
   changeSignButton.enableDOMButton();
   inputDigitButtonsDisabled = false;
@@ -107,6 +118,7 @@ function enableOperatorAndCommandButtons() {
   operatorButtons.forEach((button) => {
     button.enableDOMButton();
   });
+
   clearButton.enableDOMButton();
   deleteButton.enableDOMButton();
   equalButton.enableDOMButton();
@@ -117,9 +129,11 @@ function disableOperatorAndCommandButtons(disableClear) {
   operatorButtons.forEach((button) => {
     button.disableDOMButton();
   });
+
   if (disableClear) {
     clearButton.disableDOMButton();
   }
+
   deleteButton.disableDOMButton();
   equalButton.disableDOMButton();
   operatorAndCommandButtonsDisabled = true;
@@ -130,8 +144,10 @@ function changeInputNumberSign() {
     selectedNum = "";
     displayCleanNeeded = false;
   }
+
   if (selectedNum[0] === "-") {
     selectedNum = selectedNum.slice(1, selectedNum.length);
+
     if (inputDigitButtonsDisabled) {
       enableDigitInputButtons();
       reachedMaxDigits = false;
@@ -140,7 +156,9 @@ function changeInputNumberSign() {
     if (selectedNum.length === 0) {
       selectedNum = "0";
     }
+
     selectedNum = "-" + selectedNum;
+
     if (selectedNum.length === maxDigits) {
       reachedMaxDigits = true;
       disableDigitInputButtons();
@@ -160,6 +178,7 @@ function selectOperator(op) {
     commaButton.enableDOMButton();
     enableDigitInputButtons();
   }
+
   if (selectedOperator === "") {
     selectedOperator = op;
     operatorButtons.get(op).highlightDOMButton();
@@ -174,10 +193,12 @@ function executeClear() {
   enableDigitInputButtons();
   selectedNum = "0";
   calcDisplay.updateDisplayOutput(selectedNum);
+
   if (selectedOperator != "") {
     operatorButtons.get(selectedOperator).removeHighlightDOMButton();
     selectedOperator = "";
   }
+
   hasComma = false;
   reachedMaxDigits = false;
   finishedInputFirstNumber = false;
@@ -187,16 +208,20 @@ function executeClear() {
 function executeDelete() {
   if (selectedNum.length > 0 && selectedNum != "-0") {
     selectedNum = selectedNum.slice(0, selectedNum.length - 1);
+
     if (!selectedNum.includes(",") && hasComma) {
       hasComma = false;
       commaButton.enableDOMButton();
     }
+
     if (selectedNum.length === 0) {
       selectedNum = "0";
     } else if (selectedNum === "-") {
       selectedNum = "-0";
     }
+
     calcDisplay.updateDisplayOutput(selectedNum);
+
     if (inputDigitButtonsDisabled) {
       enableDigitInputButtons();
       reachedMaxDigits = false;
@@ -206,8 +231,10 @@ function executeDelete() {
 
 function executeEqual() {
   let operationExecuted = true;
+
   if (finishedInputFirstNumber && !displayCleanNeeded) {
     num2 = parseFloat(selectedNum.replace(",", "."));
+
     switch (selectedOperator) {
       case "+":
         result = sum(num1, num2);
@@ -230,7 +257,9 @@ function executeEqual() {
     result = parseFloat(selectedNum.replace(",", "."));
     operationExecuted = false;
   }
+
   printFormattedOutput(result);
+
   if (operationExecuted) {
     blockAllButtonsButClear();
   }
@@ -239,9 +268,11 @@ function executeEqual() {
 function printFormattedOutput(result) {
   if (result != null) {
     let formattedResult = String(result).replace(".", ",");
+
     if (formattedResult.length > maxDigits) {
       formattedResult = String(result.toExponential(2).replace(".", ","));
     }
+
     calcDisplay.updateDisplayOutput(formattedResult);
   } else {
     calcDisplay.showErrorMessage("ERROR");
@@ -256,11 +287,13 @@ function blockAllButtonsButClear() {
 
 function addNumberInputDOMButtonEventListener() {
   const buttons = document.querySelectorAll("button[data-type = number]");
+
   buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
       let clickedNum = event.target.textContent;
       enterDigit(clickedNum);
     });
+
     const numButton = new Button(button);
     numButtons.push(numButton);
   });
@@ -268,29 +301,35 @@ function addNumberInputDOMButtonEventListener() {
 
 function addClearInputDOMButtonEventListener() {
   const clearButton = document.querySelector("button[data-type = clear]");
+
   clearButton.addEventListener("click", (event) => {
     executeClear();
   });
+
   const button = new Button(clearButton);
   return button;
 }
 
 function addDeleteInputDOMButtonEventListener() {
   const delButton = document.querySelector("button[data-type=delete]");
+
   delButton.addEventListener("click", (event) => {
     executeDelete();
   });
+
   const button = new Button(delButton);
   return button;
 }
 
 function addOperatorInputDOMButtonEventListener() {
   const operators = document.querySelectorAll("button[data-type = operator]");
+
   operators.forEach((button) => {
     button.addEventListener("click", (event) => {
       let clickedOp = event.target.textContent;
       selectOperator(clickedOp);
     });
+
     const opButton = new OperatorButton(button);
     operatorButtons.set(button.textContent, opButton);
   });
@@ -298,30 +337,34 @@ function addOperatorInputDOMButtonEventListener() {
 
 function addEqualInputDOMButtonEventListener() {
   const equalButton = document.querySelector("button[data-type = equal]");
+
   equalButton.addEventListener("click", (event) => {
     executeEqual();
   });
+
   const button = new Button(equalButton);
   return button;
 }
 
 function addCommaInputDOMButtonEventListener() {
   const commaButton = document.querySelector("button[data-type = comma]");
+
   commaButton.addEventListener("click", (event) => {
     let clickedNum = event.target.textContent;
     enterDigit(clickedNum);
   });
+  
   const button = new Button(commaButton);
   return button;
 }
 
 function addChangeSignInputDOMButtonEventListener() {
-  const changeSignButton = document.querySelector(
-    "button[data-type = changeSign]"
-  );
+  const changeSignButton = document.querySelector("button[data-type = changeSign]");
+
   changeSignButton.addEventListener("click", (event) => {
     changeInputNumberSign();
   });
+  
   const button = new Button(changeSignButton);
   return button;
 }
