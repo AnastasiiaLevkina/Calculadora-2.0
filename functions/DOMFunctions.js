220385fce63e9285543e954c6ffed6d0dafdc987
AnastasiiaLevkina <anastasiia.levkina.7e5@itb.cat>
2024-06-27 13:30:43 +0200

    13.30

---
+++ functions/DOMFunctions.js
@@ -0,0 +1,85 @@
+function addNumberInputDOMButtonEventListener() {
+  const buttons = document.querySelectorAll("button[data-type = number]");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let clickedNum = event.target.textContent;
+      enterDigit(clickedNum);
+    });
+
+    const numButton = new Button(button);
+    numButtons.push(numButton);
+  });
+}
+
+function addClearInputDOMButtonEventListener() {
+  const clearButton = document.querySelector("button[data-type = clear]");
+
+  clearButton.addEventListener("click", (event) => {
+    executeClear();
+  });
+
+  const button = new Button(clearButton);
+  return button;
+}
+
+function addDeleteInputDOMButtonEventListener() {
+  const delButton = document.querySelector("button[data-type=delete]");
+
+  delButton.addEventListener("click", (event) => {
+    executeDelete();
+  });
+
+  const button = new Button(delButton);
+  return button;
+}
+
+function addOperatorInputDOMButtonEventListener() {
+  const operators = document.querySelectorAll("button[data-type = operator]");
+
+  operators.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      let clickedOp = event.target.textContent;
+      selectOperator(clickedOp);
+    });
+
+    const opButton = new OperatorButton(button);
+    operatorButtons.set(button.textContent, opButton);
+  });
+}
+
+function addEqualInputDOMButtonEventListener() {
+  const equalButton = document.querySelector("button[data-type = equal]");
+
+  equalButton.addEventListener("click", (event) => {
+    executeEqual();
+  });
+
+  const button = new Button(equalButton);
+  return button;
+}
+
+function addCommaInputDOMButtonEventListener() {
+  const commaButton = document.querySelector("button[data-type = comma]");
+
+  commaButton.addEventListener("click", (event) => {
+    let clickedNum = event.target.textContent;
+    enterDigit(clickedNum);
+  });
+
+  const button = new Button(commaButton);
+  return button;
+}
+
+function addChangeSignInputDOMButtonEventListener() {
+  const changeSignButton = document.querySelector(
+    "button[data-type = changeSign]"
+  );
+
+  changeSignButton.addEventListener("click", (event) => {
+    changeInputNumberSign();
+  });
+
+  const button = new Button(changeSignButton);
+  return button;
+}
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let result = 0;
 let selectedOperator = "";
 let selectedNum = "";
 
-const maxDigits = 9;
+const MAX_DIGITS = 9;
 const display = document.querySelector("#display");
 const calcDisplay = new Display(display);
 
@@ -55,7 +55,8 @@ function enterDigit(digit) {
 }
 
 function controlInputNumberCoherency() {
-  const hasExtraCharacters = selectedNum.length > String(parseFloat(selectedNum)).length;
+  const hasExtraCharacters =
+    selectedNum.length > String(parseFloat(selectedNum)).length;
   const isNotNegativeZero = selectedNum != "-0";
   const hasNoDecimals = !selectedNum.includes(",");
 
@@ -283,88 +284,4 @@ function blockAllButtonsButClear() {
   disableDigitInputButtons();
   disableOperatorAndCommandButtons(false);
   changeSignButton.disableDOMButton();
-}
-
-function addNumberInputDOMButtonEventListener() {
-  const buttons = document.querySelectorAll("button[data-type = number]");
-
-  buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      let clickedNum = event.target.textContent;
-      enterDigit(clickedNum);
-    });
-
-    const numButton = new Button(button);
-    numButtons.push(numButton);
-  });
-}
-
-function addClearInputDOMButtonEventListener() {
-  const clearButton = document.querySelector("button[data-type = clear]");
-
-  clearButton.addEventListener("click", (event) => {
-    executeClear();
-  });
-
-  const button = new Button(clearButton);
-  return button;
-}
-
-function addDeleteInputDOMButtonEventListener() {
-  const delButton = document.querySelector("button[data-type=delete]");
-
-  delButton.addEventListener("click", (event) => {
-    executeDelete();
-  });
-
-  const button = new Button(delButton);
-  return button;
-}
-
-function addOperatorInputDOMButtonEventListener() {
-  const operators = document.querySelectorAll("button[data-type = operator]");
-
-  operators.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      let clickedOp = event.target.textContent;
-      selectOperator(clickedOp);
-    });
-
-    const opButton = new OperatorButton(button);
-    operatorButtons.set(button.textContent, opButton);
-  });
-}
-
-function addEqualInputDOMButtonEventListener() {
-  const equalButton = document.querySelector("button[data-type = equal]");
-
-  equalButton.addEventListener("click", (event) => {
-    executeEqual();
-  });
-
-  const button = new Button(equalButton);
-  return button;
-}
-
-function addCommaInputDOMButtonEventListener() {
-  const commaButton = document.querySelector("button[data-type = comma]");
-
-  commaButton.addEventListener("click", (event) => {
-    let clickedNum = event.target.textContent;
-    enterDigit(clickedNum);
-  });
-  
-  const button = new Button(commaButton);
-  return button;
-}
-
-function addChangeSignInputDOMButtonEventListener() {
-  const changeSignButton = document.querySelector("button[data-type = changeSign]");
-
-  changeSignButton.addEventListener("click", (event) => {
-    changeInputNumberSign();
-  });
-  
-  const button = new Button(changeSignButton);
-  return button;
 }
